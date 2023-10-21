90fb1e9441464601a607a3abc7a5dcebf98256a8
dstankus <domantas.stankus@cec.valantic.com>
2023-10-21 13:45:53 +0300

    BinarySearch algo implemented

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,21 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let lowPoint = 0;
+    let highPoint = haystack.length
 
+    do {
+        const midPoint = Math.floor((lowPoint + highPoint) / 2)
+        
+        if (haystack[midPoint] === needle) {
+            return true;
+        }
+
+        if (haystack[midPoint] > needle) {
+            highPoint = midPoint
+        } else {
+            lowPoint = midPoint + 1
+        }
+
+    } while (lowPoint < highPoint)
+
+    return false;
 }
